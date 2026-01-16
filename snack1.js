9d553f39fafb7ff928d26c216773aeecc859e15f
BrunoMaiolo <brunosj@hotmail.it>
2026-01-16 14:36:26 +0100

    creo un array di bici con nome e peso

---
+++ snack1.js
@@ -0,0 +1,8 @@
+// array di bici//
+const bikes = [
+{ nome: "Bianchi", peso: 10},
+{ nome: "Atala", peso: 12},
+{ nome: "Carrera", peso: 8},
+{ nome: "Piaggio", peso: 15},
+{ nome: "Pininfarina", peso: 11}, 
+];
