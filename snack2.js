f7a739050a4d514315bdc1a986ac51089c9a9442
BrunoMaiolo <brunosj@hotmail.it>
2026-01-16 14:59:43 +0100

    creo un array di team con punti e falli subiti

---
+++ snack2.js
@@ -0,0 +1,10 @@
+//array di squadre//
+const teams = [
+    {nome: "Inter", punti: 46, falliSubiti: 27  },
+    {nome: "Milan", punti: 43, falliSubiti: 40},
+    {nome: "Napoli", punti: 40, falliSubiti: 20},
+    {nome: "Juventus", punti: 39, falliSubiti: 25},
+    {nome: "Roma", punti: 39, falliSubiti: 33}
+];
+
+
